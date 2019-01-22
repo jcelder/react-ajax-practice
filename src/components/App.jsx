80cc278b886ced4a70d28575c81fd49834c96161
Mandy Trinh <saphiric@gmail.com>
2019-01-21 22:19:39 -0800

    Complete assignment with ajax call

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import $ from 'jquery';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       name: '',
-      message: ''
+      message: '',
+      response: ''
       };
+    // creates lexical scope binding for `this` keyword
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleMessageChange = this.handleMessageChange.bind(this);
@@ -19,26 +22,40 @@ class App extends React.Component {
   }
   handleSubmit(evt) {
     evt.preventDefault();
-    alert('Message ' + this.state.message + ' was submitted by ' + this.state.name)
+    const server = 'http://ec2-13-57-25-101.us-west-1.compute.amazonaws.com:3000/api/hrsf111/greeting';
+    let msgObject = {'name': this.state.name, 'message': this.state.message};
+    $.ajax({
+      url: server,
+      type: 'POST',
+      data: JSON.stringify(msgObject),
+      contentType: 'application/json',
+      // use fat arrow syntax to avoid losing `this` binding in setState function
+      success: (resultFromServer) => {
+        this.setState({response: resultFromServer});
+      },
+      error: function(errorMsg) {
+        console.log(errorMsg);
+      }
+    });
   }
 
   render() {
     return (
       <div>
+        <div>
+          Returned value: {this.state.response}
+        </div>
         <form onSubmit={this.handleSubmit}>
           <label>
             First Name:
-            <input type="text" value={this.state.value} onChange={this.handleNameChange} />
+            <input type="text" value={this.state.name} onChange={this.handleNameChange} />
           </label>
           <label>
             Message:
-            <input type="text" value={this.state.value} onChange={this.handleMessageChange} />
+            <input type="text" value={this.state.message} onChange={this.handleMessageChange} />
           </label>
           <input type="submit" value="Send Message"  />
         </form>
-        <div>
-          Returned value: {this.state.name} {this.state.message}
-        </div>
       </div>
     );
   }
